e0ceb78f044b440fac97c2009db38b192808ef50
João Vitor de Sousa <joaovjoaovitor@edu.unifor.br>
2025-12-04 12:02:46 -0300

    :tada: init: Versão inicial do projeto

---
+++ src/main/java/br/com/bibliotecaunifor/BibliotecauniforApplication.java
@@ -0,0 +1,13 @@
+package br.com.bibliotecaunifor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecauniforApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecauniforApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bibliotecaunifor/BibliotecauniforApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bibliotecaunifor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecauniforApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
